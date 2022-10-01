e43fa463815301ff0e873cfeb52cd2a6cee89b5c
SlowSailKnowNothing <slowsail@mail.ustc.edu.cn>
2022-10-01 21:08:02 +0800

    本来aop应该失效但是好像还是好使不知道为啥可能要问一下

--- LearnSpring/src/main/java/org/example/service/MailService.java
+++ LearnSpring/src/main/java/org/example/service/MailService.java
@@ -1,6 +1,7 @@
 package org.example.service;
 
 
+import org.example.annotation.TestAnno;
 import org.springframework.stereotype.Component;
 
 import java.time.ZoneId;
@@ -15,15 +16,16 @@ public class MailService {
         this.zoneId=id;
     }
 
+    @TestAnno("MailService#getTime")//这个注解不起作用？
     public String getTime() {
         return ZonedDateTime.now(this.zoneId).format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
     }//这个时间的写法还需要进一步学习一下
 
     public void sendLoginEmail(){
-        System.out.println("send login Email");
+        System.out.println("send login Email:"+getTime());
     }
 
     public void sendRegisterEmail(){
-        System.out.println("send register Email");
+        System.out.println("send register Email"+getTime());
     }
 }
